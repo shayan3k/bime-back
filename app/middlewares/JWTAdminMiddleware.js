544d019efe20f3eeb51b352e4b9ff3777c298681
Shayan <shayan3k@gmail>
2020-08-05 01:35:44 +0430

    Middleware updated

--- app/middlewares/JWTAdminMiddleware.js
+++ app/middlewares/JWTAdminMiddleware.js
@@ -2,9 +2,9 @@ const db = require("../models");
 
 module.exports = async (req, res, next) => {
   //should be change for deployment setup
-  // const user = await db.User.findOne({ phone_number: req.user.phone_number });
-  const user = await db.User.findOne({ phone_number: "09120000000" });
-  req.user = user;
+  const user = await db.User.findOne({ phone_number: req.user.phone_number });
+  // const user = await db.User.findOne({ phone_number: "09120000000" });
+  // req.user = user;
 
   if (!user)
     return res.json({
--- app/middlewares/JWTStudentMiddleware.js
+++ app/middlewares/JWTStudentMiddleware.js
@@ -3,9 +3,9 @@ const db = require("../models");
 module.exports = async (req, res, next) => {
   try {
     //should be change for deployment setup
-    // const user = await db.User.findOne({ phone_number: req.user.phone_number });
-    const user = await db.User.findOne({ phone_number: "09127170126" });
-    req.user = user;
+    const user = await db.User.findOne({ phone_number: req.user.phone_number });
+    // const user = await db.User.findOne({ phone_number: "09127170126" });
+    // req.user = user;
 
     if (!user)
       return res.json({
